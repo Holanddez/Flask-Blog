bf8150b7e5ff6fd43b778044f9c01eb94eea70d0
Holanddez <elfosupremo@hotmail.com>
2022-06-27 16:14:49 -0300

    Add app.run

--- main.py
+++ main.py
@@ -201,5 +201,5 @@ def delete_post(post_id):
     db.session.commit()
     return redirect(url_for('get_all_posts'))
 
-#if __name__ == "__main__":
-#    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
